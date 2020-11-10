1b1b354950f307afcd402d9ad331a0c879fc1604
DouglasRfV <douglas.rfvianna@gmail.com>
2020-11-10 01:45:34 -0300

    iniciando models

--- kanbanWebApp/admin.py
+++ kanbanWebApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (
+    Setor,
+    Produto
+)
 
-# Register your models here.
+admin.site.register(Setor)
+admin.site.register(Produto)
--- kanbanWebApp/models.py
+++ kanbanWebApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Setor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nome
