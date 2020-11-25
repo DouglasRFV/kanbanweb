3cec438f076ecc070bd23f34aabb8d78f7ea0f95
DouglasRfV <douglas.rfvianna@gmail.com>
2020-11-25 20:26:49 -0300

    deploy heroku

--- kanbanWeb/settings.py
+++ kanbanWeb/settings.py
@@ -79,12 +79,6 @@ WSGI_APPLICATION = 'kanbanWeb.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
