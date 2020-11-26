fb716e28858f2e7e70cc98f35004f7bbf9e185b0
DouglasRfV <douglas.rfvianna@gmail.com>
2020-11-26 01:50:37 -0300

    tentativa deploy heroku

--- kanbanWeb/settings.py
+++ kanbanWeb/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'kanbanWeb.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+DATABASES = { 
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl), 
+}
 
 
 # Password validation
