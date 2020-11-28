46d93c8d5eba25929dc775863abc58ccca747f11
DouglasRfV <douglas.rfvianna@gmail.com>
2020-11-27 23:10:27 -0300

    iniciando cruds

--- kanbanWebApp/urls.py
+++ kanbanWebApp/urls.py
@@ -3,10 +3,14 @@ from .views import (
     home,
     painel, 
     setor,
+    produto_novo,
+    produto_update,
 )    
 
 urlpatterns = [
     path('', home, name='kanbanWebApp_home'),
     path('painel/<int:id>/', painel, name='kanbanWebApp_painel'),
     path('setor/', setor, name='kanbanWebApp_setor'),
+    path('produto-novo/<int:id>', produto_novo, name='kanbanWebApp_produto_novo'),
+    path('produto-update/<int:id>/', produto_update, name='kanbanWebApp_produto_update'),
 ]
--- kanbanWebApp/views.py
+++ kanbanWebApp/views.py
@@ -5,6 +5,7 @@ from .models import (
 )
 import json
 from django.core import serializers
+from .forms import ProdutoForm
 
 def home(request):
     return render(request, 'kanbanWebApp/index.html')
@@ -13,12 +14,38 @@ def setor(request):
     if request.method == 'POST':
         idSetor = request.POST['selectSetor']
         produtos = Produto.objects.filter(setor=idSetor)
-        data = {'produtos': produtos, 'idSetor': idSetor}
+        form = ProdutoForm()
+        data = {'produtos': produtos, 'idSetor': idSetor, 'form': form}
         return render(request, 'kanbanWebApp/setor.html', data)
     else:
         produtos = Produto.objects.all()
         data = {'produtos': produtos}
         return render(request, 'kanbanWebApp/setor.html', data)
+
+def produto_novo(request, id):
+    form = ProdutoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    idSetor = id
+    produtos = Produto.objects.filter(setor=idSetor)
+    form = ProdutoForm()
+    data = {'produtos': produtos, 'idSetor': idSetor, 'form': form}
+    return render(request, 'kanbanWebApp/setor.html', data)
+
+def produto_update(request, id):
+    print(setor)
+    data = {}
+    produto = Produto.objects.get(id=id)
+    form = ProdutoForm(request.POST or None, instance=produto)
+    data['produto'] = produto
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('kanbanWebApp_setor')
+    else:
+        return render(request, 'kanbanWebApp/update_produto.html', data)     
 
 def painel(request, id):
     #data = {'listaProd': json.dumps(list(produto))}
